93acf73d03c72b726b6af7abb261052bd450f0cc
NaoRYK <dragonarcher2005@gmail.com>
2026-04-16 13:59:45 -0300

    aksjas

---
+++ PryEDChalimondC/clsCola.cs
@@ -0,0 +1,59 @@
+﻿
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PryEDChalimondC
+{
+    internal class clsCola
+    {
+        private clsNodo pri;
+        private clsNodo ult;
+
+
+        public clsNodo Primero
+        {
+            get { return pri; }
+            set { pri = value; }
+        }
+
+        public clsNodo Ultimo
+        {
+            get { return ult; }
+            set { ult = value; }
+        }
+
+        public void Agregar(clsNodo nuevo)
+        {
+            if(Primero == null)
+            {
+                Primero = nuevo;
+                Ultimo = nuevo;
+            }
+            else
+            {
+                Ultimo.Siguiente = nuevo;
+                Ultimo = nuevo;
+            }
+        }
+
+        public void Eliminar(clsNodo eliminar)
+        {
+            if(Primero == null)
+            {
+                Primero = eliminar;
+                Ultimo = eliminar;
+
+            }
+            else
+            {
+                Primero = Primero.Siguiente;
+            }
+        }
+    }
+}
---
+++ PryEDChalimondC/clsNodo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PryEDChalimondC
+{
+    internal class clsNodo
+    {
+        private Int32 cod;
+        private String nom;
+        private String tram;
+        private clsNodo sig;
+        private clsNodo ant;
+
+
+        public Int32 Codigo
+        {
+            get { return cod; }
+            set { cod = value; }
+        }
+        public String Nombre
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        public String Tramite
+        {
+            get { return tram; }
+            set { tram = value; }
+        }
+
+        public clsNodo Siguiente
+        {
+            get { return sig; }
+            set { sig = value; }
+
+        }
+        public clsNodo Anterior
+        {
+            get { return ant; }
+            set { ant = value; }
+
+        }
+    }
+}
